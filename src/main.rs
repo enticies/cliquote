bfae74c19f4484e640175ed1672e61a23c8e861d
Ismayil Jabbarli <ismayl@nordicresults.com>
2023-10-01 23:38:09 +0200

    Implement "random_quote" function.

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,43 @@ extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use std::env;
+use serde::Deserialize;
 
-use reqwest::Error;
+use reqwest::{Client, Error};
+
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct ResponseObject {
+    content: String,
+    author: String,
+}
+
 
 #[async_std::main]
 async fn main() {
-    random_quote().await;
+    let acceptable_arguments = ["-c", "--category"];
+    let args: Vec<String> = env::args().collect();
 
+    let mut response_object: ResponseObject;
+
+    if args.len() != 1 && args.len() != 3 {
+        println!("Incorrect number of arguments.");
+    }
+
+
+    match args.len() {
+        1 => response_object = random_quote().await.unwrap(),
+        _ => unreachable!()
+    };
+
+    println!("\"{}\"\n{}", response_object.content, response_object.author);
 }
 
-async fn random_quote() -> Result<(), Error> {
-    let response = reqwest::get("https://api.quotable.io/random").await?;
+async fn random_quote() -> Result<ResponseObject, Error> {
+    let response: ResponseObject = Client::new().get("https://api.quotable.io/random").send().await?.json().await?;
 
-    println!("Status: {}", response.status());
 
-    let body = response.text().await?;
-    println!("Body:\n{}", body);
-
-    Ok(())    
+    Ok(response)
 }
