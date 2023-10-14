d81423dd63978182f9c67854974487b708e2ea17
Ismayil Jabbarli <ismayl@nordicresults.com>
2023-10-14 23:32:25 +0200

    Progress.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 
-use std::env;
 use serde::Deserialize;
+use std::env;
+use std::process;
 
-use reqwest::{Client, Error};
-
+use reqwest::{Client, Response};
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -15,30 +15,54 @@ struct ResponseObject {
     author: String,
 }
 
-
 #[async_std::main]
 async fn main() {
     let acceptable_arguments = ["-c", "--category"];
     let args: Vec<String> = env::args().collect();
 
-    let mut response_object: ResponseObject;
+    let response_object: ResponseObject;
 
-    if args.len() != 1 && args.len() != 3 {
+    if args.len() < 1 || args.len() > 3 {
         println!("Incorrect number of arguments.");
     }
 
+    if args.len() == 1 {
+        response_object = random_quote().await.json().await.unwrap();
+    } else if args.len() == 2 {
+        if args[1] == "-h" || args[1] == "--here" {
+            print_help();
+        }
+    } else if args.len() == 3 {
+        if !acceptable_arguments.contains(&args[1].as_str()) {
+            println!(
+                "Incorrect usage of flags. Use -h or --help to learn more about flags you can use."
+            );
+            process::exit(1);
+        }
+    } else {
+        unreachable!();
+    }
 
-    match args.len() {
-        1 => response_object = random_quote().await.unwrap(),
-        _ => unreachable!()
-    };
-
-    println!("\"{}\"\n{}", response_object.content, response_object.author);
+    // println!(
+    //     "\"{}\"\n{}",
+    //     response_object.content, response_object.author
+    // );
 }
 
-async fn random_quote() -> Result<ResponseObject, Error> {
-    let response: ResponseObject = Client::new().get("https://api.quotable.io/random").send().await?.json().await?;
+async fn random_quote() -> Response {
+    let response: Result<Response, reqwest::Error> = Client::new()
+        .get("https://api.qotable.io/random")
+        .send()
+        .await;
 
+    if response.is_err() {
+        println!("Couldn't fetch the quote for some reason. Check your internet connection.");
+        process::exit(1);
+    }
 
-    Ok(response)
+    response.unwrap()
+}
+
+fn print_help() {
+    println!("in help function");
 }
